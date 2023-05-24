7d3413c662d6347ffc950d8c53591ec53008282b
jycouet <jycouet@gmail.com>
2023-05-24 14:47:59 +0200

    :art: NEW: layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>This demo showcase</h2>
+<ul>
+	<li>
+		<a href="https://remult.dev/" target="_blank">Remult</a>
+	</li>
+	<li>
+		<a href="https://houdinigraphql.com/" target="_blank">Houdini</a>
+	</li>
+</ul>
