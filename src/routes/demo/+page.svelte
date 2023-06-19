c7a36d0a039bcb562bf22a3fa7294233a99cb697
jycouet <jycouet@gmail.com>
2023-06-19 15:48:16 +0200

    :construction_worker: UPDATE: node adaptor

---
+++ src/routes/demo/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { browser } from '$app/environment'
+  import { remultLive } from '$lib/remult-svelte/remultLive'
+  import { Task } from '$shared/Task'
+  import { remult } from 'remult'
+
+  const repo = remult.repo(Task)
+
+  const list = remultLive(repo)
+  $: browser && list.listen({ limit: 3 })
+</script>
+
+{#each $list as task}
+  <div>{task.title}</div>
+{/each}
